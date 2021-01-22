737c236883f75ecae820b97e04aa67d9517940a6
Lucas Sales <lucassales398@gmail.com>
2021-01-22 15:18:47 -0300

    Dashboard: Pegando o user logado e controlando rotas autenticadas

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,9 +4,33 @@
 </template>
 
 <script>
+import { watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import services from "./services";
 import ModalFactory from "./components/ModalFactory";
 
 export default {
   components: { ModalFactory },
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+
+    watch(
+      () => route.path,
+      async () => {
+        if (route.meta.hasAuth) {
+          const token = window.localStorage.getItem("token");
+
+          if (!token) {
+            router.push({ name: "Home" });
+            return;
+          }
+
+          const { data } = await services.users.getMe();
+          console.log(data);
+        }
+      }
+    );
+  },
 };
 </script>
