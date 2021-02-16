2b77b8e006cca6c20ff056a360b981a7356b75d0
Lucas Sales <lucassales398@gmail.com>
2021-02-16 01:27:09 -0300

    Widget: criação do botão flutuante que abre o Widget

---
+++ widget/src/components/Icon/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script>
+import Loading from "./Loading.vue";
+import Copy from "./Copy.vue";
+import ChevronDown from "./ChevronDown.vue";
+import Chat from "./Chat.vue";
+import Close from "./Close.vue";
+import ArrowRight from "./ArrowRight.vue";
+
+export default {
+  components: {
+    Loading,
+    Copy,
+    ChevronDown,
+    Chat,
+    Close,
+    ArrowRight,
+  },
+  props: {
+    name: { type: String, required: true },
+  },
+};
+</script>
