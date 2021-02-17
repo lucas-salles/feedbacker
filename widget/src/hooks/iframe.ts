2bdad06404d431b4f0b89dd6fdaceb83ccf58825
Lucas Sales <lucassales398@gmail.com>
2021-02-17 01:58:04 -0300

    Widget: criação camada de serviço e request de criação de feedback

--- widget/src/hooks/iframe.ts
+++ widget/src/hooks/iframe.ts
@@ -21,7 +21,7 @@ export default function useIframeControl(): IframeControl {
     }
 
     setCurrentPage("https://playground-url.com");
-    setApiKey("api_key_de_teste");
+    setApiKey("fcd5015c-10d3-4e9c-b395-ec7ed8850165");
     setFingerprint("123123123123123");
   }
 
