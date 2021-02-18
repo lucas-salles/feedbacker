a3bddce04f71cdb4dd34c7cd1f4d9175e50c5785
Lucas Sales <lucassales398@gmail.com>
2021-02-17 23:22:33 -0300

    correção no footer de dashboard para pegar sempre o ano atual

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -2,12 +2,14 @@
   <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
   <contact />
   <div class="flex justify-center py-10 bg-brand-gray">
-    <p class="font-medium text-center text-gray-800">feedbacker &copy; 2021</p>
+    <p class="font-medium text-center text-gray-800">
+      feedbacker &copy; {{ state.currentYear }}
+    </p>
   </div>
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { onMounted, reactive } from "vue";
 import { useRouter } from "vue-router";
 import CustomHeader from "./CustomHeader";
 import Contact from "./Contact";
@@ -18,6 +20,10 @@ export default {
   setup() {
     const router = useRouter();
     const modal = useModal();
+
+    const state = reactive({
+      currentYear: new Date().getFullYear(),
+    });
 
     onMounted(() => {
       const token = window.localStorage.getItem("token");
@@ -39,6 +45,7 @@ export default {
     }
 
     return {
+      state,
       handleLogin,
       handleAccountCreate,
     };
